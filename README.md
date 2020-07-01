7b827347ce91bd97a5f52b27ee80a4b97df1234f
tianyi <styzf@qq.com>
2020-07-01 19:42:12 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# 360插件化之之宿主启动插件Activity
+
+[TOC]
+
+该项目基于：[360插件化之宿主调用插件方法](https://github.com/tianyalu/XxtPlugin)
---
+++ childplugin/src/main/java/com/sty/xxt/xxtplugin/childplugin/Test.java
@@ -0,0 +1,9 @@
+package com.sty.xxt.xxtplugin.childplugin;
+
+import android.util.Log;
+
+public class Test {
+    public static void print() {
+        Log.e("sty", "这是子模块方法中的打印信息");
+    }
+}
